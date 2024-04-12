50051d23ea1cb2b6fd9a784172a770cca9076649
Magdalena Draszczyk <magda.draszczyk@gmail.com>
2024-04-12 14:14:54 +0100

    Solved + bonus

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,12 +1,14 @@
 class Chronometer {
   constructor() {
+   
     this.currentTime = 0;
     this.intervalId = null;
   }
 
   // Function is an object. Empty object is true. To check if function is passed we need to check if it exists meaning if it's true. We cannot do ==/=== true cause it will try to convert the function to a boolean value (truthy or falsy). To check if function exists / is invoked it's enough to do if (printTimeCallback) {}
-  
+
   start(printTimeCallback) {
+  
     this.intervalId = setInterval(() => {
       this.currentTime++
       if(printTimeCallback) printTimeCallback()
@@ -15,14 +17,17 @@ class Chronometer {
 
 
   getMinutes() {
+   
     return Math.floor(this.currentTime/60)
   }
 
   getSeconds() {
+
     return (this.currentTime % 60)
   }
 
   computeTwoDigitNumber(value) {
+
     return ("0" + value).slice(-2)
     /* const valueString = value.toString()
     let convertedValue = 0 
@@ -32,14 +37,17 @@ class Chronometer {
   }
 
   stop() {
+
     clearInterval(this.intervalId)
   }
 
   reset() {
+
     this.currentTime = 0
   }
 
   split() {
+
     let minutes = this.computeTwoDigitNumber(this.getMinutes())
     let seconds = this.computeTwoDigitNumber(this.getSeconds())
     return `${minutes}:${seconds}`
